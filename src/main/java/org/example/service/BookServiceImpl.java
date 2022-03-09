36bef9b9fedd08e8db74244079585882d710aabd
何逸诚 <1424864061@qq.com>
2022-03-09 23:26:55 +0800

    Initial commit

---
+++ src/main/java/org/example/service/BookServiceImpl.java
@@ -0,0 +1,35 @@
+package org.example.service;
+
+import org.example.mapper.BookMapper;
+import org.example.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService {
+    private BookMapper bookMapper;
+
+    //    service调用mapper层
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books book) {
+        return bookMapper.addBook(book);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
